ff49b330ec1f5360c9c4fcc17e705e4baccd448b
achiles12 <achiles12@yahoo.com>
2024-07-31 19:33:00 +0800

    002 backend done

---
+++ python/tutorial/03_note/backend/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path("notes/", views.NoteListCreate.as_view(), name="note-list"),
+    path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
+]
